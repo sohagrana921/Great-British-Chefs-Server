a18ef2c382028959c9f62e4b6e99a442131d2ecb
Sohag Rana <sohagrana921@gmail.com>
2023-05-02 13:50:32 +0600

    recipes data done

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 4000;
-
+const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Assignment-10 server is running");
 });
 
-// app.get('/categories', (req, res) => {
-//     res.send(categories);
-// })
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
 
-// app.get('/news', (req, res) => {
-//     res.send(news);
-// })
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
 
 // app.get('/news/:id', (req, res) => {
 //     const id = req.params.id;
