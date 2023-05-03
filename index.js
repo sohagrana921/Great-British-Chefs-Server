f4bc02226149d7ed8388b814afa1554812a0100e
Sohag Rana <sohagrana921@gmail.com>
2023-05-04 00:12:16 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 4000;
 const chefs = require("./data/chefs.json");
 const recipes = require("./data/recipes.json");
+const newest = require("./data/newestrecipes.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -17,24 +18,9 @@ app.get("/chefs", (req, res) => {
 app.get("/recipes", (req, res) => {
   res.send(recipes);
 });
-
-// app.get('/news/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedNews = news.find(n => n._id === id);
-//     res.send(selectedNews)
-// })
-
-// app.get('/categories/:id', (req, res) => {
-//     const id = parseInt(req.params.id);
-//     if (id === 0) {
-//         res.send(news)
-//     }
-//     else {
-//         const categoryNews = news.filter(n => parseInt(n.category_id) === id);
-//         res.send(categoryNews)
-//     }
-
-// })
+app.get("/newestrecipes", (req, res) => {
+  res.send(newest);
+});
 
 app.listen(port, () => {
   console.log(`Assignment-10 API is running on port: ${port}`);
